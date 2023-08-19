24a5317303d704c737251c4402441ec24d0c9f95
pramethg <pramethg@gmail.com>
2023-08-18 20:16:41 -0400

    constrained ae updates: training

--- unmix_constrained.py
+++ unmix_constrained.py
@@ -7,22 +7,27 @@ from torch.utils.data import DataLoader
 import warnings
 import numpy as np
 from tqdm import trange
-from datasets import ZCA
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
-from datasets import SingleCholesterolDataset
 warnings.filterwarnings('ignore')
 
+from utils import *
+from datasets import ZCA
+from datasets import SingleCholesterolDataset
+
 class AutoEncoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim, activation = 'trelu'):
+    def __init__(self, input_dim, hidden_dim, activation = 'trelu', tied = True):
         super(AutoEncoder, self).__init__()
-        self.encw = nn.Parameter(torch.randn(input_dim, hidden_dim))
+        self.tied = tied
+        encw = wtscale(symdecorrelation(torch.randn(input_dim, hidden_dim)))
+        self.encw = nn.Parameter(encw)
         self.encb = nn.Parameter(torch.zeros(hidden_dim))
-        self.decw = self.encw.t()
+        self.decw = self.encw.t() if self.tied else nn.Parameter(torch.randn(hidden_dim, input_dim))
         self.decb = nn.Parameter(torch.zeros(input_dim))
 
-        self.alpha = nn.Parameter(torch.randn(hidden_dim))
-        self.beta = nn.Parameter(torch.randn(hidden_dim))
+        if activation[0] == 't':
+            self.alpha = nn.Parameter(torch.randn(hidden_dim))
+            self.beta = nn.Parameter(torch.randn(hidden_dim))
 
         activations = {'trelu': self.trelu, 'tsigmoid': self.tsigmoid, 'tsoftplus': self.tsoftplus, 'sigmoid': F.sigmoid, 'relu': F.relu, 'softplus': F.softplus, 'tanh': F.tanh}
         assert activation in activations, 'Pick Valid Activation Function'
@@ -43,12 +48,13 @@ class AutoEncoder(nn.Module):
     def forward(self, x):
         x = torch.matmul(x, self.encw) + self.encb
         encout = self.activation(x)
-        decout = F.sigmoid(torch.matmul(x, self.encw.t()) + self.decb)
-        return encout, decout    
+        decout = F.sigmoid(torch.matmul(encout, self.encw.t()) + self.decb) if self.tied \
+                else F.sigmoid(torch.matmul(encout, self.decw) + self.decb)
+        return encout, decout
 
-def test():
+def test(tied = True):
     x = torch.randn(2048, 10)
-    model = AutoEncoder(10, 3, 'tsigmoid')
+    model = AutoEncoder(10, 3, 'tsigmoid', tied)
     model.eval()
     assert model.encw.all() == model.decw.t().all()
     print(model.forward(x)[0].shape)
@@ -69,19 +75,29 @@ def icaupdate(W, encoded):
     W = symdecorrelation(W)
     return W
 
-def unmixing(save = False, seed = 9, beta = 0.5, zcatest = True):
+def wtscale(W):
+    W = (W - W.min()) / (W.max() - W.min())
+    Wsum = torch.sum(W, axis = 1)
+    for idx in range(W.shape[0]):
+        W[idx] /= Wsum[idx]
+    return W
+
+def unmixing(seed = 9, beta = 1, negexp = 1.0, save = False):
     BATCH_SIZE = 396*101
-    EPOCHS = 300
-    LRATE = 3e-4
+    EPOCHS = 150
+    LRATE = 7e-3
     NCOMP = 3
-    wavelist = []
-    torch.manual_seed(seed = seed)
-    data = SingleCholesterolDataset(root = './data/hb_hbo2_fat_11_20', wavelist = [], depth = [20], whiten = 'zca', normalize = True)
+    SEED = 9
+    negexp = 1.2
+    np.random.seed(seed = seed)
+    torch.manual_seed(seed = SEED)
+    data = SingleCholesterolDataset(root = './data/hb_hbo2_fat_11', wavelist = 'EXP10', depth = [20], whiten = 'zca', normalize = True)
     dataloader = DataLoader(data, batch_size = BATCH_SIZE, shuffle = False, num_workers = 16)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = AutoEncoder(len(data.wavelist), NCOMP).to(device = device)
+    model = AutoEncoder(len(data.wavelist), NCOMP, activation = 'sigmoid', tied = True).to(device = device)
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr = LRATE)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode = 'min', factor = 0.5, patience = 10, verbose = True)
     losses = []
     model.train()
     for epoch in (t := trange(EPOCHS)):
@@ -92,14 +108,19 @@ def unmixing(save = False, seed = 9, beta = 0.5, zcatest = True):
             optimizer.zero_grad()
             encoded, decoded = model.forward(batch)
             mse = criterion(decoded, batch)
-            negentropy = -(torch.mean(-torch.exp(-(encoded ** 2) / 2)) - torch.mean(-torch.exp(-(torch.randn_like(encoded)) ** 2 / 2)))
-            loss = (beta * mse) + ((1 - beta) * negentropy)
+            negentropy = torch.abs(-(torch.mean(-torch.exp(- negexp * (encoded ** 2) / 2)) - torch.mean(-torch.exp(- negexp * (torch.randn_like(encoded)) ** 2 / 2))))
+            loss = ((beta * mse) + ((1 - beta) * negentropy)) if beta != 1 else (mse + negentropy)
             loss.backward()
             optimizer.step()
-            epochloss.append(loss)
+            epochloss.append(loss.item())
+            t.update(1)
             t.set_description_str(f'EPOCH: [{epoch + 1}/{EPOCHS}]')
-            t.set_postfix_str(f'LOSS: {loss.item()}')
-        losses.append(sum(epochloss) / len(epochloss))
+            t.set_postfix_str(f'MSELOSS: {mse.item():.3f} NEGENTROPY: {negentropy.item():.3f} TOTAL LOSS: {loss.item():.3f}')
+        epochlossmean = sum(epochloss) / len(epochloss)
+        scheduler.step(epochlossmean)
+        losses.append(epochlossmean)
+        if epochlossmean < 0.02:
+            break
     
     if save:
         torch.save(model.state_dict(), 'CONSTRAINED.pth')
@@ -109,16 +130,31 @@ def unmixing(save = False, seed = 9, beta = 0.5, zcatest = True):
     plt.savefig('./CONSTRAINED/Loss.png', dpi = 500)
     plt.close()
 
-    sim_data = np.array([np.array(loadmat(f'./data/hb_hbo2_fat_29_11/PA_Image_{wave}.mat')['Image_PA']) for wave in wavelist])
+    testdata = SingleCholesterolDataset(root = './data/hb_hbo2_fat_11', wavelist = 'EXP10', depth = [20], whiten = 'zca', normalize = True)
+    preds = np.array(model.encode(testdata[0].to(device)).cpu().detach()).reshape((396, 101, NCOMP))
+    
+    plot_comps_2d(preds, data.wavelist, model.encw.detach().cpu().numpy(), order = [0, 1, 2], xticks = data.wavelist, title = 'CONSTRAINED AE', save = './CONSTRAINED/Encoder')
+
+    simdata = testdata[0].numpy().reshape((396, 101, len(testdata.wavelist)))
+    decout = model.forward(testdata[0].to(device))[1].detach().cpu().numpy().reshape((396, 101, len(testdata.wavelist)))
+    plt.figure(figsize = (10, 6))
+    for idx in range(len(testdata.wavelist)):
+        plt.subplot(2, 10, idx + 1)
+        plt.imshow(simdata[:, :, idx], cmap = "hot")
+        plt.colorbar()
+        plt.subplot(2, 10, idx + 11)
+        plt.imshow(decout[:, :, idx], cmap = "hot")
+        plt.colorbar()
+    plt.savefig('./CONSTRAINED/Decoder.png')
+
+if __name__ == "__main__":
+    unmixing(seed = 9, beta = 1, negexp = 1.2, save = False)
+
+"""
+    sim_data = np.array([np.array(loadmat(f'./data/hb_hbo2_fat_11_20/PA_Image_{wave}.mat')['Image_PA']) for wave in data.wavelist])
     c, h, w = sim_data.shape
     sim_data = sim_data.transpose((1, 2, 0)).reshape((h*w, c))
     zca = ZCA()
-    sim_data = ZCA
+    sim_data = zca.fit_transform(sim_data)
     sim_data = torch.tensor(np.expand_dims(sim_data, axis = 0), dtype = torch.float32)
-    preds = np.array(model.encoder(sim_data.to(device)).cpu().detach())[0].reshape((h, w, NCOMP))
-    
-    wts = (model.encoder[0].weight).cpu().detach().numpy()
-    plot_comps_2d(preds, wave_list, np.linalg.pinv(wts), order = [0, 1, 2], xticks = wave_list, title = 'FCAE', save = 'fcae')
-
-if __name__ == "__main__":
-    test()
+"""
