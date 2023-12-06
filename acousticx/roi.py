3f564cb84758b24c161bd1b76889916b70b0e7be
Prameth Gaddale <pramethg@gmail.com>
2023-12-06 16:47:47 -0500

    added changes to the jpytr nptebook

--- acousticx/roi.py
+++ acousticx/roi.py
@@ -6,7 +6,8 @@ import warnings
 warnings.filterwarnings('ignore')
 
 if __name__ == "__main__":
-    pafile = loadmat("../../acousticx/data/beamformed/11062023-ft_760_even_915_odd_co2_30_90_30_OBP_Laser_PA_1477.mat")['beamformed']
+    pafile = loadmat("../../acousticx/data/TEST.mat")['beamformed']
+    print(pafile.shape)
     plt.figure(figsize = (15, 5))
-    plt.imshow(pafile[10, :, :], cmap = "hot")
+    plt.imshow(pafile[100:, :, 100], cmap = "hot")
     plt.show()
